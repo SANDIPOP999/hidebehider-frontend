f5decebf8eada6aa55ed83c08683bd73e53e7e22
SANDIPOP999 <106296204+SANDIPOP999@users.noreply.github.com>
2025-02-17 18:32:56 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,11 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      input: 'index.html'
+      input: 'index.html', // Not usually necessary unless you're dealing with multiple entry points
     }
   },
   server: {
-    port: 3000
+    port: 3000,  // Customize the port if needed
+    open: true   // Optionally open the app automatically in your browser
   }
 });
