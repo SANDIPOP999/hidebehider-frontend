f3c65cafcb4c2853bef67d2a8bc58e9b3467043b
SANDIPOP999 <106296204+SANDIPOP999@users.noreply.github.com>
2025-02-17 23:02:08 +0530

    Update App.js

--- main/App.js
+++ main/App.js
@@ -1,21 +1,14 @@
-import React, { useEffect } from "react";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Panel from "./pages/Panel";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
-  useEffect(() => {
-    console.log("✅ App Mounted!");
-  }, []);
-
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/panel" element={<Panel />} />
         <Route path="*" element={<h1>404 Not Found</h1>} />
       </Routes>
     </Router>
